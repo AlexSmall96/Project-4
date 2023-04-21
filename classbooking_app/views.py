27e0f0cc46cbca9a765dcd6930c53418ad388707
AlexSmall96 <alex.small739@gmail.com>
2023-04-21 17:18:59 +0000

    "Update views.py to handle multiple bookings"

--- classbooking_app/views.py
+++ classbooking_app/views.py
@@ -11,18 +11,22 @@ def get_bookings(request):
     return render(request, 'classbooking_app/bookings.html', context)
 
 
+activities = {'boxfit':'10:00', 'kettlebells':'11:00', 'yoga':'12:00'}
+
 # Load make_bookings.html
 # Create instance of booking from form data
 def make_booking(request):
     if request.method == "POST":
-        name = request.POST.get('booking_name')
-        Booking.objects.create(
-            name=name,
-            date='2023-05-01',
-            time='10:00',
-            spaces=25,
-            location='Studio A',
-            running=True
-        )
+        for act in activities:
+            if request.POST.get(act) == 'on':
+                Booking.objects.create(
+                    name=act,
+                    date='2023-05-01',
+                    time=activities[act],
+                    spaces=25,
+                    location='Studio A',
+                    user='test_user_3',
+                    running=True
+                    )
         return redirect('get_bookings')
     return render(request, 'classbooking_app/make_booking.html')
