7a39ec2ee700d79aef09ac03d6c0cc735d3e613a
AlexSmall96 <alex.small739@gmail.com>
2023-04-21 16:28:01 +0000

    "Remove commas from Booking model"

--- classbooking_app/models.py
+++ classbooking_app/models.py
@@ -5,12 +5,12 @@ from datetime import date, datetime
 
 
 class Booking(models.Model):
-    name = models.CharField(max_length=50, null=False, blank=False),
-    date = models.DateField(default=date.today()),
-    time = models.TimeField(default=datetime.now()),
-    spaces = models.IntegerField(),
-    location = models.CharField(max_length=50, null=False, blank=False),
-    user = models.CharField(max_length=50, null=False, blank=False),
+    name = models.CharField(max_length=50, null=False, blank=False)
+    date = models.DateField(default=date.today())
+    time = models.TimeField(default=datetime.now())
+    spaces = models.IntegerField()
+    location = models.CharField(max_length=50, null=False, blank=False)
+    user = models.CharField(max_length=50, null=False, blank=False)
     running = models.BooleanField(default=True)
 
     def __str__(self):
