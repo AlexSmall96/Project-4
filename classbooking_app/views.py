2036c77b8a186965e3ec90ab4399509c8aabd283
AlexSmall96 <alex.small739@gmail.com>
2023-04-21 15:46:01 +0000

    "Migrate Booking model"

--- classbooking_app/views.py
+++ classbooking_app/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from .models import Booking
-# Create your views here.
 
+
+# Load basic html template
 def get_bookings(request):
     bookings = Booking.objects.all()
     context = {
-        'bookings' : bookings
+        'bookings': bookings
     }
     return render(request, 'classbooking_app/bookings.html', context)
 
+
+# Load make_bookings.html
+# Create instance of booking from form data
 def make_booking(request):
     return render(request, 'classbooking_app/make_booking.html')
