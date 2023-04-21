6666cca117c826fe08c73139c29fbbebbd466e8c
AlexSmall96 <alex.small739@gmail.com>
2023-04-21 16:41:54 +0000

    "Setup form to add bookings to database"

--- classbooking_app/views.py
+++ classbooking_app/views.py
@@ -14,4 +14,15 @@ def get_bookings(request):
 # Load make_bookings.html
 # Create instance of booking from form data
 def make_booking(request):
+    if request.method == "POST":
+        name = request.POST.get('booking_name')
+        Booking.objects.create(
+            name=name,
+            date='2023-05-01',
+            time='10:00',
+            spaces=25,
+            location='Studio A',
+            running=True
+        )
+        return redirect('get_bookings')
     return render(request, 'classbooking_app/make_booking.html')
