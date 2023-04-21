19e86e8ea0fab3d3f2bbc0ffd743e9873fbb3749
AlexSmall96 <alex.small739@gmail.com>
2023-04-21 14:59:00 +0000

    "Create Booking model"

--- classbooking_app/models.py
+++ classbooking_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import date, datetime
 
 # Create your models here.
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False),
+    date = models.DateField(default=date.today()),
+    time = models.TimeField(default=datetime.now()),
+    spaces = models.IntegerField(),
+    location = models.CharField(max_length=50, null=False, blank=False),
+    user = models.CharField(max_length=50, null=False, blank=False),
+    running = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
