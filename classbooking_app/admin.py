6c903bbde0511617ec078af7936a75e74419fa22
AlexSmall96 <alex.small739@gmail.com>
2023-04-22 14:18:31 +0000

    "Create Session model and populate from data.json"

--- classbooking_app/admin.py
+++ classbooking_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Booking
+from .models import Booking, Session
 
 
 admin.site.register(Booking)
+admin.site.register(Session)
--- classbooking_app/models.py
+++ classbooking_app/models.py
@@ -15,3 +15,15 @@ class Booking(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Session(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False, default="test_class")
+    date = models.DateField(default=date.today())
+    time = models.TimeField(default=datetime.now())
+    spaces = models.IntegerField(default=25)
+    location = models.CharField(max_length=50, default='Studio A')
+    running = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
