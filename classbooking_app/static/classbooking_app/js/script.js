e7c5bae1b9d6f735c3d6354729882d6b1e57b97f
AlexSmall96 <alex.small739@gmail.com>
2023-04-22 19:44:56 +0000

    "Convert date to serial format in script.js"

--- classbooking_app/static/classbooking_app/js/script.js
+++ classbooking_app/static/classbooking_app/js/script.js
@@ -1,4 +1,19 @@
-document.addEventListener("DOMContentLoaded",function() {
-    console.log('js file linked correctly')
-});
+document.addEventListener("DOMContentLoaded", function(){
+    // Get date chosen from form
+    let currentDate = document.getElementById("select-date")
+    // Add event listener on date form to convert date into serial number
+    currentDate.addEventListener("change", function(){
+        let date = new Date(this.value);
+        let serialDate = Math.floor(date.getTime()/(1000*60*60*24))+25569;
+        // Convert serial number into string and add class number (1 for boxfit)
+        let serialStr = serialDate.toString();
+        let session_id = Number("1".concat(serialStr));
+        console.log(session_id)
+        
+        
     
+     
+
+
+    })
+})
