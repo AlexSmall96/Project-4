f2e5caae226feb63c1e3a50513a7b1596af7816e
AlexSmall96 <alex.small739@gmail.com>
2023-05-06 16:36:48 +0000

    "Create bookings data when box is ticked"

--- classbooking_app/static/classbooking_app/js/script.js
+++ classbooking_app/static/classbooking_app/js/script.js
@@ -6,39 +6,36 @@ document.addEventListener("DOMContentLoaded", function(){
     let currentDate = document.getElementById("select-date")
     let formCompleted = document.getElementById("form-complete")
     let form = document.getElementById("date-form")
+    let cart = document.getElementById("cart")
+    let cartData = {}
+    let addBoxes = document.getElementsByClassName("add-to-cart")
+    //for (let id of cartIds){
+        //tickbox = document.getElementById(id)
+        //console.log(id, tickbox)
+    //}
     // Add event listener on date input to convert date into serial number
     currentDate.addEventListener("change", function(){
         let date = new Date(this.value);
-        let serialDate = Math.floor(date.getTime()/(1000*60*60*24))+25569;
+        //let serialDate = Math.floor(date.getTime()/(1000*60*60*24))+25569;
         // Convert serial number into string and add class number (1 for boxfit)
-        let serialStr = serialDate.toString();
-        let session_id = Number("1".concat(serialStr));
+        //let serialStr = serialDate.toString();
+        //let session_id = Number("1".concat(serialStr));
         form.submit()
     })
     // Add event listener to each checkbox
-    let addBoxes = document.getElementsByClassName("add-to-cart")
-    let cart = document.getElementById("cart")
+    
     let checkoutList = document.getElementById("checkout-list")
     for (let box of addBoxes){
         box.addEventListener("change", function(){
             oldCart = cart.value
             if (this.checked){
                 // Display session id in cart
-                currentDate = document.getElementById("select-date")
                 cart.value = oldCart.concat(box.id).concat(" ")
-                let newRow = document.createElement("div")
                 let location = box.parentNode.previousSibling.previousSibling
                 let activity = location.previousSibling.previousSibling.previousSibling.previousSibling
                 let time = location.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling
-                newRow.classList.add("row")
-                newRow.innerHTML = `
-                <div class="col">${activity.innerHTML}</div>
-                <div class="col">${currentDate.value}</div>
-                <div class="col">${time.innerHTML}</div>
-                <div class="col">${location.innerHTML}</div>                
-                `
-                checkoutList.appendChild(newRow)
-                console.log(box.parentNode.previousSibling.previousSibling.innerHTML)
+                cartData[box.id] = [location.innerHTML, currentDate.value, activity.innerHTML, time.innerHTML]
+                console.log(cartData)
             }
         })
     }
@@ -48,9 +45,25 @@ document.addEventListener("DOMContentLoaded", function(){
     let finalisedBox = document.getElementById("finalised")
 
     checkoutButton.addEventListener("click", () => {
+        
+        let cartIds = cart.value.split(" ")
+        cartIds.pop()
+        for (let i of cartIds){
+            console.log(cartData[i])
+             let newRow = document.createElement("div")
+             newRow.classList.add("row")
+             newRow.innerHTML = `
+             <div class="col">${cartData[i][0]}</div>
+             <div class="col">${cartData[i][1]}</div>
+             <div class="col">${cartData[i][2]}</div>
+             <div class="col">${cartData[i][3]}</div>                
+             `
+             checkoutList.appendChild(newRow)
+        }
         timetable.style.display="none"
         confirmBookings.style.display = "block"
         finalisedBox.value = "y"
+
     })
     }
 })
