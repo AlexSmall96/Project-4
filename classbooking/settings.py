5057d76fa6b241cd60ed9ceb42f13c09f02e6f88
AlexSmall96 <alex.small739@gmail.com>
2023-05-21 11:04:32 +0000

    "Update col sizes on session timetable"

--- classbooking/settings.py
+++ classbooking/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-alexsmall96-project4-z4wrtww3ucz.ws-eu97.gitpod.io']
+ALLOWED_HOSTS = ['8000-alexsmall96-project4-z4wrtww3ucz.ws-eu97.gitpod.io', 'localhost']
 
 
 # Application definition
