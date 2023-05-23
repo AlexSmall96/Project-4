2c9883631adf2d7f568de2016781cfa148875fd5
AlexSmall96 <alex.small739@gmail.com>
2023-05-23 21:07:58 +0000

    "Update col structure for images on members area"

--- classbooking_app/static/classbooking_app/js/view_bookings.js
+++ classbooking_app/static/classbooking_app/js/view_bookings.js
@@ -1,23 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(){
-        const cancelButtons = document.getElementsByClassName("cancel-button")
         const cancelForm = document.getElementById("cancel-form")
         const cancelledBookBtns = document.getElementsByClassName("run-False-btn")
-        const confirmModal = document.getElementById("confirm-modal")
-        const confirmModalBtn = document.getElementById("confirm-modal-btn")
         const feedbackBtn = document.getElementById("members-feedback-btn")
         const feedbackModal = document.getElementById("members-feedback-modal")
         let yesButtons = document.getElementsByClassName("confirm-cancel")
         let cancelField = document.getElementById("cancel")
         let feedField = document.getElementById("members-feedback-div")
+        const bookCount = document.getElementById("bookings-count").innerHTML
+        const myBookings = document.getElementById("my-bookings")
+        const membersArea = document.getElementById("members-area")
+
+        myBookings.style.height =  `${53*bookCount}px`
+        membersArea.style.height = `${53*bookCount+200}px`
 
         for (let button of yesButtons){
             let sessionId = button.id.substring(0,6)
-            let modalId = "modal-".concat(sessionId)
-            let modal = document.getElementById(modalId)
-            let modalHeaderId = "modal-header-".concat(sessionId)
-            let modalHeader = document.getElementById(modalHeaderId)
-            let noButton = document.getElementById(sessionId.concat("-no"))
-            button.addEventListener("click", async function(){
+            button.addEventListener("click", () => {
                 cancelField.value = sessionId
                 cancelForm.submit()
             })
