2574e16b2f9e2fcc0b52ac0f7b5e1f04c5098912
AlexSmall96 <alex.small739@gmail.com>
2023-05-29 12:41:03 +0000

    "Correct order of static settings"

--- classbooking/settings.py
+++ classbooking/settings.py
@@ -19,13 +19,13 @@ if os.path.isfile("env.py"):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'classbooking/classbooking_app/templates')
-STATIC_URL = 'classbooking/classbooking_app/static/'
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'classbooking_app/templates')
+
 
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'classbooking/classbooking_app/static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'classbooking_app/static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_URL = '/static/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -135,7 +135,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
